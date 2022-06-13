d0d8f333f9a123033e4d0a1b11b0e9455fd9a451
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-06-13 16:38:42 +0200

    trying to make it work ...

---
+++ srcs/requirements/wordpress/conf/conf.sh
@@ -0,0 +1,15 @@
+#	Download wordpress
+wp core download --allow-root
+
+#	Generate config file and set up db
+wp config create --dbname=$WORDPRESS_DB_HOST --dbuser=$WORDPRESS_DB_USR \
+--dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$MYSQL_HOST --allow-root
+
+#	Install
+wp core install --url=$DOMAIN_NAME/wordpress --title=$TITLE \
+	--admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD \
+	--skip-email --admin_email=info@example.com --allow-root
+
+wp user create $WP_USR usr@example.com --role=author --user_pass=$WP_PWD --allow-root
+
+/usr/sbin/php-fpm7 -F -R
