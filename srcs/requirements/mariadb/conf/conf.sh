6438615126f3928926b29b3239ba98d9807daf0b
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-06-23 16:39:11 +0200

    mariadb ok ?

--- srcs/requirements/mariadb/conf/conf.sh
+++ srcs/requirements/mariadb/conf/conf.sh
@@ -1,11 +1,12 @@
-chown -R mysql:mysql /run/mysqld
 mysql_install_db --user=mysql \
 	--datadir=/var/lib/mysql
 
-mysqld --user=mysql << EOF
-
+cat << EOF > tfile
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
 CREATE DATABASE $MYSQL_DATABASE;
 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED by '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
+EOF
 
+mysqld --user=mysql < tfile
