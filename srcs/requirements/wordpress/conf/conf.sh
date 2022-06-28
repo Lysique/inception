23b3e646a609bf01cc78eda890a413f99ae14d32
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-06-28 15:13:34 +0200

    tests

--- srcs/requirements/wordpress/conf/conf.sh
+++ srcs/requirements/wordpress/conf/conf.sh
@@ -1,22 +1,18 @@
 sleep 3
-if [ ! -f "wp-config.php" ]; then
-	#	Download wordpress
-	wp core download --allow-root
+#	Download wordpress
+wp core download --allow-root
 
-	#	Generate config file and set up db
-	wp config create --dbname=$WORDPRESS_DB_NAME \
-	--dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASS \
-	--dbhost=$WORDPRESS_DB_HOST --allow-root
+#	Generate config file and set up db
+wp config create --dbname=$WORDPRESS_DB_NAME \
+--dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASS \
+--dbhost=$WORDPRESS_DB_HOST --allow-root
 
-	#	Install
-	wp core install --url=$URL --title=$TITLE \
-		--admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD \
-		--skip-email --admin_email=$ADMIN_MAIL --allow-root 
+#	Install
+wp core install --url=$URL --title=$TITLE \
+	--admin_user=$ADMIN_USER --admin_password=$ADMIN_PWD \
+	--skip-email --admin_email=$ADMIN_MAIL --allow-root 
 
-	wp user create $WP_USER_NAME $WP_USER_MAIL --role=author \
-	   --user_pass=$WP_USER_PASS --allow-root
-fi
-
-echo Hello $USER
+wp user create $WP_USER_NAME $WP_USER_MAIL --role=author \
+   --user_pass=$WP_USER_PASS --allow-root
 
 /usr/sbin/php-fpm7 -F -R
