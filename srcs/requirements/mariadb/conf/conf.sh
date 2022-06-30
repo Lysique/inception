154edd5f959057a6e099f82a68580ce74871cf9a
Thomas Amighi <tamighi@mi-c6-p1.s19.be>
2022-06-30 08:06:53 +0200

    gco

--- srcs/requirements/mariadb/conf/conf.sh
+++ srcs/requirements/mariadb/conf/conf.sh
@@ -3,10 +3,25 @@ mysql_install_db --basedir=/usr \
 	--user=mysql \
 	--skip-test-db
 
-mysqld --user=mysql --bootstrap < /tmp/mysqlconf.sql
+tfile=`mktemp`
+cat << EOF > $tfile
+
+USE mysql;
+FLUSH PRIVILEGES;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED by 'MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%';
+
+FLUSH PRIVILEGES;
+EOF
+
+mysqld --user=mysql --bootstrap < $tfile
 
 # allow remote connections
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
-exec mysqld --user=mysql --console
+mysqld --user=mysql --console
